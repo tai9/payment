7127e59622e76707bd79b495374642c2937d2bb1
tantai <tantai.it95@gmail.com>
2020-12-01 10:55:40 +0700

    implement react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import Invoice from "./components/Invoice/invoice";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="App">
-      <Invoice />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={() => <h1>Hello my friends 👋👋</h1>}
+          />
+          <Route exact path="/:id" children={<Invoice />} />
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
